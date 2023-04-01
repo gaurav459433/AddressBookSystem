9ec2deb54397ff93f60d0991308ef508f8f588b6
gaurav459433 <gaurav459433@gmail.com>
2023-04-01 19:02:28 +0530

    Added: Code For Creating Contact With All Parameters

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,5 +3,7 @@ package addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book Program: ");
+        Contact contact = new Contact();
+        System.out.println(contact.toString());
     }
 }
