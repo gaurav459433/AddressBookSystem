a33ea4a1e166a40d8e3a09351ab7dd044566108a
gaurav459433 <gaurav459433@gmail.com>
2023-04-01 19:13:51 +0530

    Added: Code For Edit Contact By Using Their Name

--- AddressBook.java
+++ AddressBook.java
@@ -29,4 +29,29 @@ public class AddressBook {
     public void showContact() {
         System.out.println(contact.toString());
     }
+
+    public void editContact() {
+        System.out.println("Enter First Name To Edit:");
+        String name = sc.next();
+        if (contact.getFirstName().equals(name)) {
+            System.out.println("Enter First Name: ");
+            contact.setFirstName(sc.next());
+            System.out.println("Enter Last Name: ");
+            contact.setLastName(sc.next());
+            System.out.println("Enter Address Name: ");
+            contact.setAddress(sc.next());
+            System.out.println("Enter City Name: ");
+            contact.setCity(sc.next());
+            System.out.println("Enter State Name: ");
+            contact.setState(sc.next());
+            System.out.println("Enter Email: ");
+            contact.setEmail(sc.next());
+            System.out.println("Enter Zip Code: ");
+            contact.setZip(sc.nextInt());
+            System.out.println("Enter Mobile Number: ");
+            contact.setPhoneNumber(sc.nextLong());
+        } else {
+            System.out.println("Contact Doesn't Exist:");
+        }
+    }
 }
