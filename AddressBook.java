482bc0f741d0f5431fe6c4f8882e57fe01c31614
gaurav459433 <gaurav459433@gmail.com>
2023-04-01 19:07:38 +0530

    Added: Code For Add New Contact To Address Book

---
+++ AddressBook.java
@@ -0,0 +1,32 @@
+package addressbook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+    Scanner sc = new Scanner(System.in);
+    Contact contact = new Contact();
+
+    public void addContact() {
+        System.out.println("Enter First Name: ");
+        contact.setFirstName(sc.next());
+        System.out.println("Enter Last Name: ");
+        contact.setLastName(sc.next());
+        System.out.println("Enter Address Name: ");
+        contact.setAddress(sc.next());
+        System.out.println("Enter City Name: ");
+        contact.setCity(sc.next());
+        System.out.println("Enter State Name: ");
+        contact.setState(sc.next());
+        System.out.println("Enter Email: ");
+        contact.setEmail(sc.next());
+        System.out.println("Enter Zip Code: ");
+        contact.setZip(sc.nextInt());
+        System.out.println("Enter Mobile Number: ");
+        contact.setPhoneNumber(sc.nextLong());
+
+    }
+
+    public void showContact() {
+        System.out.println(contact.toString());
+    }
+}
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,7 +3,8 @@ package addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book Program: ");
-        Contact contact = new Contact();
-        System.out.println(contact.toString());
+        AddressBook book = new AddressBook();
+        book.addContact();
+        book.showContact();
     }
 }
